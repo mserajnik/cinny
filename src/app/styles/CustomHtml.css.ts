60f778142eb40d3ed4c401a6c3cd00e2d9e16593
Michael Serajnik <m@mser.at>
2026-01-08 19:06:39 +0100

    Make custom emote sizes adjustable and fix some related styling

--- src/app/styles/CustomHtml.css.ts
+++ src/app/styles/CustomHtml.css.ts
@@ -201,8 +201,8 @@ export const EmoticonBase = style([
   {
     display: 'inline-block',
     padding: '0.05rem',
-    height: '1em',
-    verticalAlign: 'middle',
+    height: 'auto',
+    verticalAlign: 'baseline',
   },
 ]);
 
@@ -218,9 +218,7 @@ export const Emoticon = recipe({
       minWidth: '1em',
       fontSize: '1.33em',
       lineHeight: '1em',
-      verticalAlign: 'middle',
-      position: 'relative',
-      top: '-0.35em',
+      verticalAlign: 'baseline',
       borderRadius: config.radii.R300,
     },
   ],
@@ -236,7 +234,8 @@ export const Emoticon = recipe({
 export const EmoticonImg = style([
   DefaultReset,
   {
-    height: '1em',
+    height: 'var(--emoticon-size, 1em)',
+    width: 'auto',
     cursor: 'default',
   },
 ]);
